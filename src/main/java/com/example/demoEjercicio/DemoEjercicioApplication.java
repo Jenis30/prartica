82eca735857dcdc00dd7f738cbc5a136eb2ecdc9
jennys jhoana guzman martinez <jhoana030guzman@gmail.com>
2023-10-10 11:57:00 -0500

    primer commit

---
+++ src/main/java/com/example/demoEjercicio/DemoEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoEjercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEjercicioApplication.class, args);
+	}
+
+}
